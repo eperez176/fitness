873284cc1177fe1f58787194f632437fffe15339
emperez <emperez@bu.edu>
2022-05-07 20:46:44 -0500

    home page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: false}),
   ],
   providers: [],
   bootstrap: [AppComponent]
